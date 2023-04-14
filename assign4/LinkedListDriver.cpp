21a558b98018b9f6ac90fb6c1a7a70cc3c5cd126
jmidani <125365919+jmidani@users.noreply.github.com>
2023-04-14 07:19:32 +0200

    Update LinkedListDriver.cpp

--- assign4/LinkedListDriver.cpp
+++ assign4/LinkedListDriver.cpp
@@ -13,21 +13,18 @@ int main(){
     list.print();
     list.insert(1);
     list.insert(2);
+    list.insert(3);
     list.insert(4);
-    list.insert(5);
-    list.insert(6);
     list.print();
-    //list.insert(2,3);
-    //list.print();
     int sum=list.Sum();
     cout<<"Sum is: "<<sum<<endl;
 
-    for(int i=6;i>0;i--){
+    for(int i=4;i>0;i--){
         list.remove(i);
         list.print();
     }
     vector<int>j;
-    for(int i=2;i<7;i++){
+    for(int i=2;i<5;i++){
         j.push_back(i);
     }
     j.push_back(2);
@@ -37,7 +34,7 @@ int main(){
     list.print();
     sum=list.Sum();
     cout<<"Sum is: "<<sum<<endl;
-    for(int i=6;i>0;i--){
+    for(int i=5;i>0;i--){
         list.remove(i);
         list.print();
     }
